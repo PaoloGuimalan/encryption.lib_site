8f0c773e3ea1cbf7c113d453dd0292f6bf1138e9
PaoloGuimalan <pauloportes.guimalan187@gmail.com>
2021-09-18 11:52:34 -0800

    first commit

--- src/Components/Documentation.js
+++ src/Components/Documentation.js
@@ -5,22 +5,59 @@ import {motion} from 'framer-motion';
 function Documentation() {
     return (
         <motion.div id='div_docu'
-            animate={{
-                top: "0%",
-                transition:{
-                    delay: 0,
-                    duration: 1
-                } 
-            }}
-            exit={{
-                top: "100%",
-                transition:{
-                    delay: 0,
-                    duration: 1
-                } 
-            }}
         >
-            <p id='label_docu'>Hello Docu</p>
+            <motion.div
+                animate={{
+                    opacity: 1,
+                    transition:{
+                        delay: 0,
+                        duration: 1
+                    } 
+                }}
+                exit={{
+                    opacity: 0,
+                    transition:{
+                        delay: 0,
+                        duration: 1
+                    } 
+                }}
+            id='div_under_doc'>
+                <div id='div_selection'>
+                    <table id='tbl_selection'>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <p className='p_selection'>About encryption.lib</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <p className='p_selection'>Attributes</p>
+                                    <table className='under_tbl_selection'>
+                                        <tbody>
+                                            <tr>
+                                                <td>
+                                                    <p>encrypt()</p><p>decrypt()</p>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <p className='p_selection'>Parameters</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <p className='p_selection'>Error Codes</p>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </motion.div>
         </motion.div>
     )
 }
